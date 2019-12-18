a0c5aed34aa13b57863b4807a2983d86dd6635e4
garciavictor <victor.g.s@hotmail.com>
2019-12-17 21:56:18 -0300

    Change MongoDB connection settings

--- src/index.js
+++ src/index.js
@@ -20,14 +20,11 @@ Please prevent the program from waiting indefinitely by setting the write
 concern timeout limit to 2500 milliseconds.
 */
 
-MongoClient.connect(
-  process.env.MFLIX_DB_URI,
-  // TODO: Connection Pooling
-  // Set the poolSize to 50 connections.
-  // TODO: Timeouts
-  // Set the write timeout limit to 2500 milliseconds.
-  { useNewUrlParser: true },
-)
+MongoClient.connect(process.env.MFLIX_DB_URI, {
+  useNewUrlParser: true,
+  poolSize: 50,
+  wtimeout: 2500,
+})
   .catch(err => {
     console.error(err.stack)
     process.exit(1)
